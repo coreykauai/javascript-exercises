b0a82737323949e54388acbfb388925f112a8446
Coreykauai <corey@student.dev-island.org>
2023-06-30 22:39:57 -1000

    taking the rooms temp

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (f) {
+  // take temp minus 32, * 5, / 9
+  // den round um
+  return Math.round((f - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (c) {
+  // c * 1.8 + 32
+  //roundup
+  return Math.round(((c * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };

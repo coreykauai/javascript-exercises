a436a1a10255e765ccf234fc0873c433d8674034
Coreykauai <corey@student.dev-island.org>
2023-06-30 22:16:37 -1000

    leap year division equation

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  // catch years divisible by 4 wit no remainder but
+  //while if it has remainder when dividing by 100 then no go, unless
+  // unless you can divide um by 400. bra what.
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
 
 // Do not edit below this line

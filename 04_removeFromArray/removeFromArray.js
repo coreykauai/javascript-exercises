681a8ae73c48f1606f7d071327182f0b6195dfae
Coreykauai <corey@student.dev-island.org>
2023-06-30 21:07:20 -1000

    removing things from arrays is difficult

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+// function removeFromArray(array, target) {
+//   let newArray = [];
+//   for (let count = 0; count < array.length; count++) {
+//     if (array[count] !== target) newArray.push(array[count]);
+//   }
+//   return newArray;
+// }
 
+var removeFromArray = function (array, ...args) {
+  return array.filter((val) => !args.includes(val));
 };
 
 // Do not edit below this line

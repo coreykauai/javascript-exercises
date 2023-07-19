1235083795a5d0955f8487536c1aee1cea315fc2
Coreykauai <corey@student.dev-island.org>
2023-07-18 17:12:33 -1000

    finsihed calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numarray) {
+  let result = 0;
+  for (let i = 0; i < numarray.length; i++) {
+    result += numarray[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (...args) {
+  let results = 1;
+  for (let i = 0; i < args.length; i++) {
+    results *= args[i];
+  }
+  return results;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let answer = 1;
+  for (let i = n; i > 0; i--) {
+    answer *= i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
@@ -29,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
